049a66eba632a978dd59dad4b26b4805075b38c2
Danilo Jakob <danilo.murer@gmail.com>
2020-09-09 08:36:33 +0200

    CHANGE: H2-Console is now again accessable from the server

--- src/main/java/com/danilojakob/m223/punchclock/config/WebSecurity.java
+++ src/main/java/com/danilojakob/m223/punchclock/config/WebSecurity.java
@@ -28,7 +28,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SING_UP_URL).permitAll()
-                .antMatchers(HttpMethod.GET, "/h2-console").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/*.html").permitAll()
                 .antMatchers(HttpMethod.GET, "/*.js").permitAll()
                 .anyRequest().authenticated()
