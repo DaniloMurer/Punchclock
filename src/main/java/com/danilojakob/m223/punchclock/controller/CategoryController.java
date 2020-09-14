8f879a5fb540315275eb7756f0d871f58a67e4f5
Danilo Jakob <danilo.murer@gmail.com>
2020-09-14 15:55:18 +0200

    CHANGE: Added JavaDoc to the endpoints

--- src/main/java/com/danilojakob/m223/punchclock/controller/CategoryController.java
+++ src/main/java/com/danilojakob/m223/punchclock/controller/CategoryController.java
@@ -19,12 +19,21 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    /**
+     * Get all Categories from the database
+     * @return {@link ResponseEntity} with Status Code and data
+     */
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'EMPLOYEE')")
     @GetMapping
     public ResponseEntity getAllCategories() {
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.findAll());
     }
 
+    /**
+     * Delete a category from the database
+     * @param id {@link Long} id of the Category to delete
+     * @return {@link ResponseEntity} with Status Code
+     */
     @PreAuthorize("hasAuthority('ADMINISTRATOR')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity deleteCategory(@PathVariable Long id) {
@@ -32,12 +41,22 @@ public class CategoryController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    /**
+     * Update a category
+     * @param category {@link Category} Category to update
+     * @return {@link ResponseEntity} with Status Code and updated Category
+     */
     @PreAuthorize("hasAuthority('ADMINISTRATOR')")
     @PutMapping
     public ResponseEntity updateCategory(@Valid @RequestBody Category category) {
         return ResponseEntity.status(HttpStatus.OK).body(categoryService.updateCategory(category));
     }
 
+    /**
+     * Create a new Category
+     * @param category {@link Category} category to create
+     * @return {@link ResponseEntity} with Status Code and created Category
+     */
     @PreAuthorize("hasAuthority('ADMINISTRATOR')")
     @PostMapping
     public ResponseEntity createCategory(@Valid @RequestBody Category category) {
