4a6834e48bb77f035f11017046c117d66e0d7322
Danilo Jakob <danilo.murer@gmail.com>
2020-09-14 11:15:06 +0200

    ongoing development: Category Endpoints

--- src/main/java/com/danilojakob/m223/punchclock/controller/CategoryController.java
+++ src/main/java/com/danilojakob/m223/punchclock/controller/CategoryController.java
@@ -1,9 +1,20 @@
 package com.danilojakob.m223.punchclock.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
+
+
+
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'EMPLOYEE')")
+    @GetMapping
+    public ResponseEntity getAllCategories() {
+        return null;
+    }
 }
---
+++ src/main/java/com/danilojakob/m223/punchclock/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.danilojakob.m223.punchclock.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+}
